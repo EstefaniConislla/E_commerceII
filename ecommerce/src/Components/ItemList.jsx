f9f1b7ff317416991259dcbc635454df9c8800bf
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-11-29 23:30:56 -0500

    show products

--- ecommerce/src/Components/ItemList.jsx
+++ ecommerce/src/Components/ItemList.jsx
@@ -1,4 +1,5 @@
 import "./ItemList.css";
+import ItemModal from "./Item";
 
 const ItemList = ({ product }) => {
   console.log(product); 
@@ -22,48 +23,7 @@ const ItemList = ({ product }) => {
                 >
                   View more Detail
                 </button>
-
-                <div
-                  class="modal fade"
-                  id={`id${item.id}`}
-                  tabindex="-1"
-                  aria-labelledby="exampleModalLabel"
-                  aria-hidden="true"
-                >
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">
-                          Producto
-                        </h5>
-                        <button
-                          type="button"
-                          class="btn-close"
-                          data-bs-dismiss="modal"
-                          aria-label="Close"
-                        ></button>
-                      </div>
-                      <div class="modal-body">
-                      <img src={item.image} className="card-img-top" alt="..." />
-                      <h5 className="card-title">{item.title}</h5>
-                <p className="card-text">{item.description}</p>
-                <p className="card-price">Price: S/{item.price}</p>
-                      </div>
-                      <div class="modal-footer">
-                        <button
-                          type="button"
-                          class="btn btn-secondary"
-                          data-bs-dismiss="modal"
-                        >
-                          Close
-                        </button>
-                        <button type="button" class="btn btn-primary">
-                          Agregar Carrito
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <ItemModal item={item} />
               </div>
             </div>
           ))}
